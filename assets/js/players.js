4ad351cf42b1ac9a9c7958ba2cd63fb99fd5cac5
sofi131 <sofiasanmartin131@gmail.com>
2024-05-08 20:30:11 +0200

    js arreglado pero hay que hacer que el guardar vaya solo cuando se meten datos

--- assets/js/players.js
+++ assets/js/players.js
@@ -1,11 +1,24 @@
-// Controlar el modal para mostrar mensaje de éxito
 document.addEventListener('DOMContentLoaded', () => {
+    // Controla el modal para mostrar mensaje de éxito
     const modalTrigger = document.querySelector('button[data-bs-target="#successModal"]');
+    const modalCloseButton = document.querySelector('#successModal .btn-close');
+    const modalCloseFooter = document.querySelector('#successModal .btn-secondary');
 
     if (modalTrigger) {
         modalTrigger.addEventListener('click', () => {
             const modal = new bootstrap.Modal(document.querySelector('#successModal'));
             modal.show();
         });
+    }
+
+    if (modalCloseButton && modalCloseFooter) {
+        const redirectToIndex = () => {
+            const modal = new bootstrap.Modal(document.querySelector('#successModal'));
+            modal.hide(); // Cierra el modal
+            window.location.href = 'index.php'; // Redirige a index.php
+        };
+
+        modalCloseButton.addEventListener('click', redirectToIndex); // Redirige al hacer clic en "Cerrar"
+        modalCloseFooter.addEventListener('click', redirectToIndex); // Redirige al hacer clic en "Cerrar" del footer
     }
 });
