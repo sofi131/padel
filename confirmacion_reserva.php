18991cae74f60857b890554d77bc849cb9a0e5e2
sofi131 <sofiasanmartin131@gmail.com>
2024-05-08 16:35:31 +0200

    añadiendo conf reserva

---
+++ confirmacion_reserva.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Confirmación de Reserva</title>
+    <link rel="stylesheet" href="assets/css/confirmar.css"> 
+</head>
+<body>
+    <header>
+        <a href="index.php" class="button">Inicio</a>
+    </header>
+    <h2>Reserva Confirmada</h2>
+    <p>Tu reserva se ha realizado correctamente.</p>
+    <p>¡Disfruta de tu partido de pádel!</p>
+    
+</body>
+</html>
+
+<!-- esto podría ser un popup con js --->
