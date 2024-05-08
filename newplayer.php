f8ca8f1fd305a4f59eb605e465370fa2f12fbe83
sofi131 <sofiasanmartin131@gmail.com>
2024-05-08 19:47:41 +0200

    nuevos jugadores x nico

--- newplayer.php
+++ newplayer.php
@@ -7,7 +7,7 @@ if (isset($_SESSION["username"]) && isset($_SESSION["idreserva"])) {
         $iduser = $_SESSION["iduser"];
         $idreserva = $_SESSION["idreserva"];
         $username = $_POST["username"];
-        
+
         // Verificar si hay 4 jugadores asociados a la reserva
         $sql_count_players = "SELECT COUNT(*) AS total_players FROM play WHERE idreservation = ?";
         $stmt = $conn->prepare($sql_count_players);
@@ -15,22 +15,24 @@ if (isset($_SESSION["username"]) && isset($_SESSION["idreserva"])) {
         $stmt->execute();
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
         $total_players = $row['total_players'];
-        
-        if ($total_players >= 4) {
+
+        if ($total_players == 4) {
             $error = "No se puede agregar otro jugador, ya hay 4 jugadores en esta reserva.";
             $_SESSION["error"] = $error;
             unset($_SESSION["idreserva"]);
             header("Location: ./");
+        } else {
+            // Insertar el nuevo jugador
+            $sql = 'INSERT INTO play (iduser, idreservation, username) VALUES (?, ?, ?)';
+            $result = $conn->prepare($sql);
+            $result->bindParam(1, $iduser);
+            $result->bindParam(2, $idreserva);
+            $result->bindParam(3, $username);
+            $result->execute();
         }
-        
-        // Insertar el nuevo jugador
-        $sql = 'INSERT INTO play (iduser, idreservation, username) VALUES (?, ?, ?)';
-        $result = $conn->prepare($sql);
-        $result->bindParam(1, $iduser);
-        $result->bindParam(2, $idreserva);
-        $result->bindParam(3, $username);
-        $result->execute();
-        
+
+
+
         if ($result->rowCount() == 1) {
             header('Location: players');
             exit; // Terminar el script después de redirigir
