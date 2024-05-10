de1d50dbd3ca57b4de794290d30390c2be403dbe
sofi131 <sofiasanmartin131@gmail.com>
2024-05-10 16:39:12 +0200

    nav

--- confirmacion_reserva.php
+++ confirmacion_reserva.php
@@ -11,7 +11,6 @@
 <body class="d-flex flex-column min-vh-100"> <!-- Flex para mantener el footer al fondo -->
 
     <!-- Navbar -->
-    <!-- Barra de Navegación -->
     <nav class="navbar navbar-expand-lg navbar-light bg-white sticky-top">
         <a class="navbar-brand d-flex align-items-center" href="./">
             <img src="https://assets-global.website-files.com/6127fb2c77e53513fea9657c/612d38df9b48bca5bd62f48b_padel-tech-logo.png" alt="Logo" width="200" height="auto" class="me-2">
